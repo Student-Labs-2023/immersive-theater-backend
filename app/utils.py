abce9483b77d2ff81d3384c601f950629755967c
Kirill Iost <kirilliost13@gmail.com>
2023-07-27 04:27:07 +0600

    Some refacoring and add check for existing perfomance by id

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,5 @@
 
 def int_to_str_duration(duration):
     print(duration)
-    h = duration // 3600
     m = duration // 60
-    s = duration % 60
-    return f"{h} h. {m} min. {s} sec."
+    return f"{m} min."
